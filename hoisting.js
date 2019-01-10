dfcc29c904b8a071575e45fcb86fb5d5130b5015
cerebral-map-4184 <patriciagbarrett@gmail.com>
2019-01-10 04:29:29 +0000

    Done.

--- hoisting.js
+++ hoisting.js
@@ -1,19 +1,20 @@
+var lyric = "maybe";
 function callMe() {
   console.log("I just met you...");
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
   return lyric;
-  var lyric = "maybe";
 }
+callMe() ;
 
 
 function crazy() {
   // fix the code in here:
   thisIsCrazy();
 
-  var thisIsCrazy = function (){
-    console.log("hey!!!")
+  var thisIsCrazy()()=crazy()() {
+    console.log("hey!!!"()())
   }
 }
 
